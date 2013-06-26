a65ae0e903ec43e21c43026a02e87e276a189191
Tobi Knaup <tobi@knaup.me>
2013-06-25 23:54:05 -0400

    Fix JSON

--- src/main/scala/mesosphere/chaos/example/ExampleRestModule.scala
+++ src/main/scala/mesosphere/chaos/example/ExampleRestModule.scala
@@ -1,9 +1,11 @@
 package mesosphere.chaos.example
 
 import com.google.inject.servlet.ServletModule
-import com.google.inject.Singleton
+import com.google.inject.{Provides, Singleton}
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
+import com.fasterxml.jackson.databind.ObjectMapper
 
 /**
  * @author Tobi Knaup
@@ -13,8 +15,15 @@ class ExampleRestModule extends ServletModule {
 
   protected override def configureServlets() {
     serve("/*").`with`(classOf[GuiceContainer])
-    bind(classOf[JacksonJsonProvider]).in(classOf[Singleton])
     bind(classOf[ExampleResource]).in(classOf[Singleton])
+  }
+
+  @Provides
+  @Singleton
+  def provideJacksonJsonProvider = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    new JacksonJsonProvider(mapper)
   }
 
 }
