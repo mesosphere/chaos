000fae29ce34fe574f612e44e764fff738480d34
Tobi Knaup <tobi@knaup.me>
2013-06-26 16:28:59 -0400

    Add RestModule as base class for REST apps

--- src/main/scala/mesosphere/chaos/example/ExampleRestModule.scala
+++ src/main/scala/mesosphere/chaos/example/ExampleRestModule.scala
@@ -1,37 +1,18 @@
 package mesosphere.chaos.example
 
-import com.google.inject.servlet.ServletModule
-import com.google.inject.{Scopes, Provides, Singleton}
-import com.sun.jersey.guice.spi.container.servlet.GuiceContainer
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.codahale.metrics.servlets.{MetricsServlet, PingServlet}
-import mesosphere.validation.{ConstraintViolationExceptionMapper, JacksonMessageBodyProvider}
-import javax.validation.Validation
+import com.google.inject.Scopes
+import mesosphere.chaos.http.RestModule
 
 /**
  * @author Tobi Knaup
  */
 
-class ExampleRestModule extends ServletModule {
+class ExampleRestModule extends RestModule {
 
   protected override def configureServlets() {
-    bind(classOf[PingServlet]).in(Scopes.SINGLETON)
-    bind(classOf[MetricsServlet]).in(Scopes.SINGLETON)
+    super.configureServlets()
+
     bind(classOf[ExampleResource]).in(Scopes.SINGLETON)
-    bind(classOf[ConstraintViolationExceptionMapper]).in(Scopes.SINGLETON)
-
-    serve("/ping").`with`(classOf[PingServlet])
-    serve("/metrics").`with`(classOf[MetricsServlet])
-    serve("/*").`with`(classOf[GuiceContainer])
   }
 
-  @Provides
-  @Singleton
-  def provideJacksonJsonProvider : JacksonJsonProvider = {
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    new JacksonMessageBodyProvider(mapper, Validation.buildDefaultValidatorFactory().getValidator)
-  }
 }
---
+++ src/main/scala/mesosphere/chaos/http/RestModule.scala
@@ -0,0 +1,38 @@
+package mesosphere.chaos.http
+
+import com.codahale.metrics.servlets.{MetricsServlet, PingServlet}
+import com.google.inject.{Singleton, Provides, Scopes}
+import mesosphere.validation.{JacksonMessageBodyProvider, ConstraintViolationExceptionMapper}
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import javax.validation.Validation
+import com.google.inject.servlet.ServletModule
+
+/**
+ * Base class for REST modules.
+ *
+ * @author Tobi Knaup
+ */
+
+class RestModule extends ServletModule {
+
+  protected override def configureServlets() {
+    bind(classOf[PingServlet]).in(Scopes.SINGLETON)
+    bind(classOf[MetricsServlet]).in(Scopes.SINGLETON)
+    bind(classOf[ConstraintViolationExceptionMapper]).in(Scopes.SINGLETON)
+
+    serve("/ping").`with`(classOf[PingServlet])
+    serve("/metrics").`with`(classOf[MetricsServlet])
+    serve("/*").`with`(classOf[GuiceContainer])
+  }
+
+  @Provides
+  @Singleton
+  def provideJacksonJsonProvider: JacksonJsonProvider = {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    new JacksonMessageBodyProvider(mapper, Validation.buildDefaultValidatorFactory().getValidator)
+  }
+}
