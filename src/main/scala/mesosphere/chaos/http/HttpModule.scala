fafd0896f0064e5426d679a37c7b63afbfa00f1b
Florian Leibert <flo@leibert.de>
2013-08-17 10:14:39 -0700

    Adding basic auth
    Review: https://rb.mesosphe.re/r/345

--- src/main/scala/mesosphere/chaos/http/HttpModule.scala
+++ src/main/scala/mesosphere/chaos/http/HttpModule.scala
@@ -17,6 +17,10 @@ import java.io.File
 import javax.inject.Named
 import scala.Some
 import java.util.logging.Logger
+import org.eclipse.jetty.util.security.{Password, Credential, Constraint}
+import org.eclipse.jetty.security._
+import org.eclipse.jetty.security.authentication.BasicAuthenticator
+import scala.Some
 
 /**
  * @author Florian Leibert (flo@leibert.de)
@@ -25,14 +29,24 @@ trait HttpConf extends ScallopConf {
   lazy val port = opt[Int]("http_port",
     descr = "The port to listen on for HTTP requests", default = Some(8080),
     noshort = true)
+
   lazy val httpsPort = opt[Int]("https_port",
     descr = "The port to listen on for HTTPS requests", default = Some(8443),
     noshort = true)
+
   lazy val sslKeystorePath = opt[String]("ssl_keystore_path",
     descr = "Provides the keystore, if supplied, SSL is enabled",
     default = None, noshort = true)
+
   lazy val sslKeystorePassword =  opt[String]("ssl_keystore_password",
     descr = "The password for the keystore", default = None, noshort = true)
+
+  lazy val httpCredentials =  opt[String]("http_credentials",
+    descr = "Credentials for accessing the http service." +
+      "If empty, anyone can access the HTTP endpoint. A username:password" +
+      "is expected where the username must not contain ':'",
+    default = None, noshort = true)
+
 }
 
 class HttpModule(conf: HttpConf) extends AbstractModule {
@@ -127,6 +141,54 @@ class HttpModule(conf: HttpConf) extends AbstractModule {
     handler.addServlet(classOf[DefaultServlet], "/*")
     handler.addFilter(classOf[GuiceFilter], "/*", util.EnumSet.allOf(classOf[DispatcherType]))
     handler.addEventListener(guiceServletConf)
+    if (conf.httpCredentials.isSupplied) {
+      handler.setSecurityHandler(getSecurityHandler())
+    }
     handler
   }
+
+  def getSecurityHandler(): ConstraintSecurityHandler = {
+    val constraint = new Constraint(Constraint.__BASIC_AUTH,"user");
+    constraint.setAuthenticate(true)
+
+    //TODO(FL): Make configurable
+    constraint.setRoles(Array("user", "admin"))
+
+    // map the security constraint to the root path.
+    val cm = new ConstraintMapping()
+    cm.setConstraint(constraint)
+    cm.setPathSpec("/*")
+
+    // create the security handler, set the authentication to Basic
+    // and assign the realm.
+    val csh = new ConstraintSecurityHandler()
+    csh.setAuthenticator(new BasicAuthenticator())
+    csh.setRealmName("chaos-realm")
+    csh.addConstraintMapping(cm);
+
+    val loginService = new KeyValueLoginService
+    require(conf.httpCredentials().contains(":"),
+      f"http_credentials '${conf.httpCredentials()}' " +
+        f"must contain a ':' to separate user and password.")
+    val tup = conf.httpCredentials().split(":", 2)
+
+    //TODO(*): Use a MD5 instead.
+    loginService.putUser(tup(0), new Password(tup(1)), Array("user"))
+
+    csh.setLoginService(loginService)
+    csh
+  }
+}
+
+//TODO(*): Allow alternative loading from file.
+class KeyValueLoginService()
+  extends MappedLoginService {
+
+  def loadUser(username: String): UserIdentity = {
+    return null
+  }
+
+  def loadUsers {
+  }
+
 }
