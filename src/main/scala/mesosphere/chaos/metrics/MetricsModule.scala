d0237d2916a4f59b43100310c97fe82438225927
Matthias Veit <matthias_veit@yahoo.de>
2015-08-17 16:22:23 +0200

    Add JVM based metrics

--- src/main/scala/mesosphere/chaos/metrics/MetricsModule.scala
+++ src/main/scala/mesosphere/chaos/metrics/MetricsModule.scala
@@ -1,15 +1,27 @@
 package mesosphere.chaos.metrics
 
-import com.google.inject.{ Singleton, Provides, AbstractModule }
+import java.lang.management.ManagementFactory
+
 import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.jersey.InstrumentedResourceMethodDispatchAdapter
 import com.codahale.metrics.jetty9.InstrumentedHandler
+import com.codahale.metrics.jvm.{ BufferPoolMetricSet, GarbageCollectorMetricSet, MemoryUsageGaugeSet, ThreadStatesGaugeSet }
+import com.google.inject.{ AbstractModule, Provides, Singleton }
 import org.eclipse.jetty.servlet.ServletContextHandler
 
 class MetricsModule extends AbstractModule {
 
-  def configure() {
-    bind(classOf[MetricRegistry]).asEagerSingleton()
+  override def configure(): Unit = {}
+
+  @Singleton
+  @Provides
+  def provideRegistry(): MetricRegistry = {
+    val registry = new MetricRegistry
+    registry.register("jvm.gc", new GarbageCollectorMetricSet())
+    registry.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer))
+    registry.register("jvm.memory", new MemoryUsageGaugeSet())
+    registry.register("jvm.threads", new ThreadStatesGaugeSet())
+    registry
   }
 
   @Singleton
