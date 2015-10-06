402ceba9ddf29e52611a92584647b32ded307899
Gastón Kleiman <gaston@mesosphere.io>
2015-10-06 17:27:00 +0200

    Add request time to the logs
    (cherry picked from commit eaac22215b80caf9922b982ffe4e358b67a3c472)

--- src/main/scala/mesosphere/chaos/http/ChaosRequestLog.scala
+++ src/main/scala/mesosphere/chaos/http/ChaosRequestLog.scala
@@ -9,6 +9,8 @@ class ChaosRequestLog extends NCSARequestLog {
 
   private[this] val log = Logger.getLogger(getClass.getName)
 
+  setLogLatency(true)
+
   override def write(requestEntry: String) {
     // Remove line separator because jul will add it
     log.info(requestEntry.substring(0, requestEntry.length - lineSepLength))
